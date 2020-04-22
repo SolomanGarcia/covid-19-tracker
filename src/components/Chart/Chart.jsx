74b8b5d0cc3de82ea3310737ff6a981c6eb27ae2
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-21 20:30:51 -0600

    add chart.js package

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
   const [dailyData, setDailyData] = useState({});
@@ -9,8 +12,21 @@ const Chart = () => {
       setDailyData(await fetchDailyData());
     }
 
+   console.log(dailyData); 
+
     fetchAPI();
-  });
+  }, [dailyData]);
+
+  const lineChart = (
+    dailyData[0]
+      ? (
+        <Line 
+        data={{
+          labels: '',
+          datasets: [{}, {}]
+        }}
+      />) : null
+  );
 
   return (
     <h1>Chart</h1>
